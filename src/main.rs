cbf0b2c7c5a78b9078c55fb0e0d0bc5027ad19f1
GitHub Action <action@github.com>
2022-10-11 14:02:39 +0000

    chore: bump version 0.5.1 → 0.5.2 [skip-ci]

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ pub mod storage;
 pub mod tools;
 pub mod validate;
 
-const VERSION: &str = "0.5.1";
+const VERSION: &str = "0.5.2";
 
 #[derive(Clap)]
 #[clap(version = VERSION, author = "Kacper S. <kacper@stasik.eu>")]
